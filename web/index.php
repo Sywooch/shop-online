8e46fd97057e7c28f3d1a10f6601084336f979b5
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2017-04-30 10:39:15 +0600

    режим продакшн по-умолчанию

--- web/index.php
+++ web/index.php
@@ -1,11 +1,13 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-//defined('YII_DEBUG') or define('YII_DEBUG', false);
-//defined('YII_ENV') or define('YII_ENV', 'prod');
+// delete dev.php file when deployed to production
+if (file_exists(__DIR__ . '/dev.php')) {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');

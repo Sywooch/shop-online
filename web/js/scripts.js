908ab4c79bc689c0387a5baca84333792284ec67
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2017-11-23 21:31:49 +0300

    fix ga event

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,11 +1,17 @@
 
 $(document).on('click', 'a[href^="https://alitems.com/"]', function(event) {
+    event.preventDefault();
+    
     if (ga != undefined) {
+        ga('set', 'transport', 'beacon');
         ga('send', 'event', {
             eventCategory: 'buy',
             eventAction: 'click',
             eventLabel: event.target.href,
-            transport: 'beacon'
+            transport: 'beacon',
+            hitCallback: function() {
+                window.location.href = event.target.href;
+            }
         });
     }
 });
